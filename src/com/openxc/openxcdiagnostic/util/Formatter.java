979d07da0ee9a4f43614908c951893171035b8db
Charles Reitz <cgreitz@hotmail.com>
2014-07-10 12:46:32 -0400

    Be more DRY with more generic paramters to format methods.

--- src/com/openxc/openxcdiagnostic/util/Formatter.java
+++ src/com/openxc/openxcdiagnostic/util/Formatter.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 
 import com.openxc.messages.Command;
 import com.openxc.messages.CommandResponse;
+import com.openxc.messages.DiagnosticMessage;
 import com.openxc.messages.DiagnosticRequest;
 import com.openxc.messages.DiagnosticResponse;
 import com.openxc.openxcdiagnostic.R;
@@ -29,41 +30,23 @@ public class Formatter {
         return padRight(String.valueOf(resp.getBusId()));
     }
 
-    public static String getIdOutput(DiagnosticRequest req) {
+    public static String getIdOutput(DiagnosticMessage msg) {
         return padRight("0x"
-                + Integer.toHexString(req.getId()).toUpperCase(Locale.US));
+                + Integer.toHexString(msg.getId()).toUpperCase(Locale.US));
     }
 
-    public static String getIdOutput(DiagnosticResponse resp) {
+    public static String getModeOutput(DiagnosticMessage msg) {
         return padRight("0x"
-                + Integer.toHexString(resp.getId()).toUpperCase(Locale.US));
+                + Integer.toHexString(msg.getMode()).toUpperCase(Locale.US));
     }
 
-    public static String getModeOutput(DiagnosticRequest req) {
-        return padRight("0x" + Integer.toHexString(req.getMode()).toUpperCase(Locale.US));
+    public static String getPidOutput(DiagnosticMessage msg) {
+        return msg.getPid() == null ? "" : padRight(String.valueOf(msg.getPid()));
     }
 
-    public static String getModeOutput(DiagnosticResponse resp) {
-        return padRight("0x"
-                + Integer.toHexString(resp.getMode()).toUpperCase(Locale.US));
-    }
-
-    public static String getPidOutput(DiagnosticRequest req) {
-        return req.getPid() == null ? "" : padRight(String.valueOf(req.getPid()));
-    }
-
-    public static String getPidOutput(DiagnosticResponse resp) {
-        return resp.getPid() == null ? "" : padRight(String.valueOf(resp.getPid()));
-    }
-
-    public static String getPayloadOutput(DiagnosticRequest req) {
-        return req.getPayload() == null ? "" : padRight("0x"
-                + new String(req.getPayload()));
-    }
-
-    public static String getPayloadOutput(DiagnosticResponse resp) {
-        return resp.getPayload() == null ? "" : padRight("0x"
-                + new String(resp.getPayload()));
+    public static String getPayloadOutput(DiagnosticMessage msg) {
+        return msg.getPayload() == null ? "" : padRight("0x"
+                + new String(msg.getPayload()));
     }
 
     public static String getSuccessOutput(DiagnosticResponse resp) {
@@ -88,7 +71,7 @@ public class Formatter {
     }
     
     public static String getCommandOutput(CommandResponse resp) {
-        return resp.getCommand() == null ? "" : padRight(resp.getCommand().toString());
+        return getCommandOutput(new Command(resp.getCommand()));
     }
     
     public static String getCommandOutput(Command command) {
