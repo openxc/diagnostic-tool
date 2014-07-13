2e86a59eb6e1a397ea63637535943650d6e083d2
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-07-13 18:26:54 -0400

    Guard against the current focus being null when hiding keyboard.
    This fixed a null pointer exception for me when I hit "Send Request".

--- src/com/openxc/openxcdiagnostic/diagnostic/DiagnosticActivity.java
+++ src/com/openxc/openxcdiagnostic/diagnostic/DiagnosticActivity.java
@@ -207,11 +207,13 @@ public class DiagnosticActivity extends Activity {
     
     public void hideKeyboard() {
 
-        InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        if (manager.isAcceptingText()) {
-            manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        if(getCurrentFocus() != null) {
+            InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (manager.isAcceptingText()) {
+                manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+            getCurrentFocus().clearFocus();
         }
-        getCurrentFocus().clearFocus();
     }
     
     public void setRequestCommandState(boolean displayCommands) {        
