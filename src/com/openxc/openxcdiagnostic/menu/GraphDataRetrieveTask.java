3aae54f21889910112ca398ddf45d16114331b47
Charles Reitz <cgreitz@hotmail.com>
2014-07-28 13:50:54 -0400

    Run save actions formatting.

--- src/com/openxc/openxcdiagnostic/menu/GraphDataRetrieveTask.java
+++ src/com/openxc/openxcdiagnostic/menu/GraphDataRetrieveTask.java
@@ -2,6 +2,10 @@ package com.openxc.openxcdiagnostic.menu;
 
 import java.util.TimerTask;
 
+import android.app.Activity;
+import android.util.Log;
+
+import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.openxc.NoValueException;
 import com.openxc.VehicleManager;
@@ -9,21 +13,16 @@ import com.openxc.measurements.Measurement;
 import com.openxc.measurements.SteeringWheelAngle;
 import com.openxc.measurements.UnrecognizedMeasurementTypeException;
 
-import android.app.Activity;
-import android.util.Log;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-
-
 public class GraphDataRetrieveTask extends TimerTask {
     private VehicleManager mVehicleManager;
     private GraphViewSeries mGraphSeries;
-    private Class <? extends Measurement> mMeasurementType;
+    private Class<? extends Measurement> mMeasurementType;
     private static String TAG = "GraphDataRetrieveTask";
     private int time = 0;
     private Activity mActivity;
 
-
-    public GraphDataRetrieveTask(VehicleManager vehicleService, Activity act, GraphViewSeries series, Class <? extends Measurement> measurementType) {
+    public GraphDataRetrieveTask(VehicleManager vehicleService, Activity act,
+            GraphViewSeries series, Class<? extends Measurement> measurementType) {
         mVehicleManager = vehicleService;
         mGraphSeries = series;
         mMeasurementType = measurementType;
@@ -31,27 +30,29 @@ public class GraphDataRetrieveTask extends TimerTask {
     }
 
     public void run() {
-        
-        	if (mVehicleManager != null) {
-        		mActivity.runOnUiThread(new Runnable() {
-                    public void run() {
-                    	GraphDataRetrieveTask.this.time++;
-		                try {
-		                	Measurement measurement = mVehicleManager.get(mMeasurementType);
-		                	int value = ((SteeringWheelAngle)measurement).getValue().intValue();
-		                	mGraphSeries.resetData(new GraphViewData[] {});
-		                	mGraphSeries.appendData(new GraphViewData(time, value), true, 10);
-		                }
-		                catch (UnrecognizedMeasurementTypeException e) {
-		                	Log.w(TAG, "Unrecognized Measurement Type");
-		                	e.printStackTrace();
-		                } catch (NoValueException e) {
-		                	Log.w(TAG, "No Value Available");
-		                	e.printStackTrace();
-		                } 
+
+        if (mVehicleManager != null) {
+            mActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    GraphDataRetrieveTask.this.time++;
+                    try {
+                        Measurement measurement = mVehicleManager
+                                .get(mMeasurementType);
+                        int value = ((SteeringWheelAngle) measurement)
+                                .getValue().intValue();
+                        mGraphSeries.resetData(new GraphViewData[] {});
+                        mGraphSeries.appendData(new GraphViewData(time, value),
+                                true, 10);
+                    } catch (UnrecognizedMeasurementTypeException e) {
+                        Log.w(TAG, "Unrecognized Measurement Type");
+                        e.printStackTrace();
+                    } catch (NoValueException e) {
+                        Log.w(TAG, "No Value Available");
+                        e.printStackTrace();
                     }
-        		});
-        	}
+                }
+            });
+        }
     }
-        
+
 }
--- src/com/openxc/openxcdiagnostic/menu/GrapherActivity.java
+++ src/com/openxc/openxcdiagnostic/menu/GrapherActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
@@ -34,17 +35,19 @@ public class GrapherActivity extends Activity {
     private GraphViewSeries dataSeries;
 
     private ServiceConnection mConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
+        public void
+                onServiceConnected(ComponentName className, IBinder service) {
             Log.i(TAG, "Bound to VehicleManager");
-            mVehicleManager = ((VehicleManager.VehicleBinder)service
-                    ).getService();
-            
+            mVehicleManager = ((VehicleManager.VehicleBinder) service)
+                    .getService();
+
             mIsBound = true;
-            
+
             Integer gridPosition = getIntent().getExtras().getInt("pos");
-            Class <? extends Measurement> measurementType = GridManager.getClass(gridPosition);
-            mUpdateGraphTask = new GraphDataRetrieveTask(mVehicleManager, GrapherActivity.this, dataSeries, measurementType);
+            Class<? extends Measurement> measurementType = GridManager
+                    .getClass(gridPosition);
+            mUpdateGraphTask = new GraphDataRetrieveTask(mVehicleManager,
+                    GrapherActivity.this, dataSeries, measurementType);
             mTimer = new Timer();
             mTimer.schedule(mUpdateGraphTask, 100, 1000);
         }
@@ -56,19 +59,16 @@ public class GrapherActivity extends Activity {
         }
     };
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
         Log.i(TAG, "Vehicle dashboard created");
-        
+
         // init example series data
         dataSeries = new GraphViewSeries(new GraphViewData[] {});
-         
-        GraphView graphView = new LineGraphView(
-            this // context
-            , "GraphViewDemo" // heading
+
+        GraphView graphView = new LineGraphView(this, "GraphViewDemo" // heading
         );
         graphView.addSeries(dataSeries); // data
         graphView.setScrollable(true);
@@ -77,20 +77,19 @@ public class GrapherActivity extends Activity {
         setContentView(R.layout.graphlayout);
         LinearLayout layout = (LinearLayout) findViewById(R.id.graphlayout);
         layout.addView(graphView);
-
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        bindService(new Intent(this, VehicleManager.class),
-                mConnection, Context.BIND_AUTO_CREATE);
+        bindService(new Intent(this, VehicleManager.class), mConnection,
+                Context.BIND_AUTO_CREATE);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if(mIsBound) {
+        if (mIsBound) {
             Log.i(TAG, "Unbinding from vehicle service");
             unbindService(mConnection);
             mIsBound = false;
@@ -99,16 +98,15 @@ public class GrapherActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    	if (item.getItemId() == R.id.Diagnostic) {
-    		startActivity(new Intent(this, DiagnosticActivity.class));
-    	} else if (item.getItemId() == R.id.Menu) {
-    		startActivity(new Intent(this, MenuActivity.class));
+        if (item.getItemId() == R.id.Diagnostic) {
+            startActivity(new Intent(this, DiagnosticActivity.class));
+        } else if (item.getItemId() == R.id.Menu) {
+            startActivity(new Intent(this, MenuActivity.class));
             return true;
-    	}
-    	else if (item.getItemId() == R.id.Dashboard) {
-    	    //do nothing
-    	}
-    	return super.onOptionsItemSelected(item);
+        } else if (item.getItemId() == R.id.Dashboard) {
+            // do nothing
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
