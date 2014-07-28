df7c72716df5c1d2955abc564a88148af69953f7
Charles Reitz <cgreitz@hotmail.com>
2014-07-28 13:28:00 -0400

    Restore coloring of output based on success.

--- src/com/openxc/openxcdiagnostic/diagnostic/output/OutputRow.java
+++ src/com/openxc/openxcdiagnostic/diagnostic/output/OutputRow.java
@@ -1,10 +1,13 @@
 package com.openxc.openxcdiagnostic.diagnostic.output;
 
+import java.util.ArrayList;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.openxc.messages.Command;
 import com.openxc.messages.CommandResponse;
 import com.openxc.messages.DiagnosticRequest;
@@ -34,120 +37,152 @@ public class OutputRow {
             OutputTableManager tableManager, VehicleMessage req,
             VehicleMessage resp) {
 
-        mView = (LinearLayout) context.getLayoutInflater().inflate(R.layout.diagoutputrow, null);
+        mView = (LinearLayout) context.getLayoutInflater().inflate(
+                R.layout.diagoutputrow, null);
         mTableManager = tableManager;
         mAlertManager = new ResponseDetailsAlertManager(context, req, resp);
         mResponse = resp;
         mRequest = req;
         mContext = context;
 
+        setFields();
         initButtons();
-        fillOutputResponseTable();
-        setTimestamp();
     }
-    
+
     public void setPair(final Pair pair) {
         mRequest = pair.getRequest();
         mResponse = pair.getResponse();
-        
+
         mAlertManager.refresh(mRequest, mResponse);
+        setFields();
+    }
+
+    private void setFields() {
         fillOutputResponseTable();
         setTimestamp();
     }
-    
+
     private void setTimestamp() {
         String timestampString;
         if (mResponse.getTimestamp() != null) {
-            timestampString = Utilities.epochTimeToTime(mResponse.getTimestamp());
+            timestampString = Utilities.epochTimeToTime(mResponse
+                    .getTimestamp());
         } else {
             timestampString = "0:00";
         }
-        ((TextView) mView.findViewById(R.id.outputRowTimestamp)).setText(timestampString);
+        ((TextView) mView.findViewById(R.id.outputRowTimestamp))
+                .setText(timestampString);
     }
 
     private void initButtons() {
 
         ((Button) mView.findViewById(R.id.outputMoreButton))
-        .setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAlertManager.show();
-            }
-        });
+                .setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mAlertManager.show();
+                    }
+                });
 
         ((Button) mView.findViewById(R.id.responseDeleteButton))
-        .setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTableManager.removeRow(OutputRow.this);
-            }
-        });
+                .setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mTableManager.removeRow(OutputRow.this);
+                    }
+                });
 
         ((Button) mView.findViewById(R.id.outputResendButton))
-        .setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message;
-                if (MessageAnalyzer.isDiagnosticRequest(mRequest)) {
-                    message = "Resending Request.";
-                    mContext.send(mRequest);
-                } else if (MessageAnalyzer.isCommand(mRequest)) {
-                    message = "Resending Command.";
-                    mContext.send(mRequest);
-                } else {
-                    message = "Cannot be resent...no request/command found.";
-                }
-                Toaster.showToast(mContext, message);
-            }
-        });
+                .setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String message;
+                        if (MessageAnalyzer.isDiagnosticRequest(mRequest)) {
+                            message = "Resending Request.";
+                            mContext.send(mRequest);
+                        } else if (MessageAnalyzer.isCommand(mRequest)) {
+                            message = "Resending Command.";
+                            mContext.send(mRequest);
+                        } else {
+                            message = "Cannot be resent...no request/command found.";
+                        }
+                        Toaster.showToast(mContext, message);
+                    }
+                });
     }
-    
+
     private void setOutputInfoFormat() {
-        LinearLayout infoTable = (LinearLayout) mView.findViewById(R.id.outputInfo);
+        LinearLayout infoTable = (LinearLayout) mView
+                .findViewById(R.id.outputInfo);
         int layoutId;
         if (MessageAnalyzer.isDiagnosticResponse(mResponse)) {
             layoutId = R.layout.diagresponseoutputinfo;
         } else {
             layoutId = R.layout.commandresponseoutputinfo;
         }
-        LinearLayout newView = (LinearLayout) mContext.getLayoutInflater().inflate(layoutId, null);
+        LinearLayout newView = (LinearLayout) mContext.getLayoutInflater()
+                .inflate(layoutId, null);
         infoTable.removeAllViews();
         infoTable.addView(newView);
     }
 
     private void fillOutputResponseTable() {
-        
+
         setOutputInfoFormat();
-        LinearLayout infoTable = (LinearLayout) mView.findViewById(R.id.outputInfo);
-            if (MessageAnalyzer.isDiagnosticResponse(mResponse)) {
-                DiagnosticResponse resp = (DiagnosticResponse) mResponse;
-                ((TextView) infoTable.findViewById(R.id.busValue)).setText(Formatter.getBusOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.idValue)).setText(Formatter.getIdOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.modeValue)).setText(Formatter.getModeOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.pidValue)).setText(Formatter.getPidOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.successValue)).setText(Formatter.getSuccessOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.payloadValue)).setText(Formatter.getPayloadOutput(resp));
-                ((TextView) infoTable.findViewById(R.id.valueValue)).setText(Formatter.getValueOutput(resp));
-            } else if (MessageAnalyzer.isCommandResponse(mResponse)){
-                CommandResponse cmdResponse = (CommandResponse) mResponse;
-                ((TextView) infoTable.findViewById(R.id.commandValue)).setText( 
-                        Formatter.getCommandOutput(cmdResponse));
-                ((TextView) infoTable.findViewById(R.id.commandResponseValue)).setText( 
-                        Formatter.getMessageOutput(cmdResponse));
+        LinearLayout infoTable = (LinearLayout) mView
+                .findViewById(R.id.outputInfo);
+        if (MessageAnalyzer.isDiagnosticResponse(mResponse)) {
+            DiagnosticResponse resp = (DiagnosticResponse) mResponse;
+
+            ArrayList<TextView> outputs = new ArrayList<>();
+
+            TextView busOutput = (TextView) infoTable
+                    .findViewById(R.id.busValue);
+            busOutput.setText(Formatter.getBusOutput(resp));
+            outputs.add(busOutput);
+
+            TextView idOutput = (TextView) infoTable.findViewById(R.id.idValue);
+            idOutput.setText(Formatter.getIdOutput(resp));
+            outputs.add(idOutput);
+
+            TextView modeOutput = (TextView) infoTable
+                    .findViewById(R.id.modeValue);
+            modeOutput.setText(Formatter.getModeOutput(resp));
+            outputs.add(modeOutput);
+
+            TextView pidOutput = (TextView) infoTable
+                    .findViewById(R.id.pidValue);
+            pidOutput.setText(Formatter.getPidOutput(resp));
+            outputs.add(pidOutput);
+
+            TextView successOutput = (TextView) infoTable
+                    .findViewById(R.id.successValue);
+            successOutput.setText(Formatter.getSuccessOutput(resp));
+            outputs.add(successOutput);
+
+            TextView payloadOutput = (TextView) infoTable
+                    .findViewById(R.id.payloadValue);
+            payloadOutput.setText(Formatter.getPayloadOutput(resp));
+            outputs.add(payloadOutput);
+
+            TextView valueOutput = (TextView) infoTable
+                    .findViewById(R.id.valueValue);
+            valueOutput.setText(Formatter.getValueOutput(resp));
+            outputs.add(valueOutput);
+
+            int outputColor = Formatter.getOutputColor(mContext, resp);
+            for (TextView tv : outputs) {
+                tv.setTextColor(outputColor);
             }
-    }
 
-    /*private void fillOutputTableWithSuccessDetails(LinearLayout responseTable,
-            DiagnosticResponse resp) {
-        createAndAddRowToOutput(responseTable, "payload", Formatter.getPayloadOutput(resp), resp);
-        createAndAddRowToOutput(responseTable, "value", Formatter.getValueOutput(resp), resp);
+        } else if (MessageAnalyzer.isCommandResponse(mResponse)) {
+            CommandResponse cmdResponse = (CommandResponse) mResponse;
+            ((TextView) infoTable.findViewById(R.id.commandValue))
+                    .setText(Formatter.getCommandOutput(cmdResponse));
+            ((TextView) infoTable.findViewById(R.id.commandResponseValue))
+                    .setText(Formatter.getMessageOutput(cmdResponse));
+        }
     }
-
-    private void fillOutputTableWithFailureDetails(LinearLayout responseTable,
-            DiagnosticResponse resp) {
-        createAndAddRowToOutput(responseTable, "neg. resp. code", 
-                Formatter.getOutputTableResponseCodeOutput(resp), resp);
-    }*/
 
     public LinearLayout getView() {
         return mView;
@@ -155,10 +190,12 @@ public class OutputRow {
 
     public Pair getPair() {
         if (MessageAnalyzer.isDiagnosticResponse(mResponse)) {
-            return new DiagnosticPair(mRequest == null ? null : (DiagnosticRequest) mRequest,
+            return new DiagnosticPair(mRequest == null ? null
+                    : (DiagnosticRequest) mRequest,
                     (DiagnosticResponse) mResponse);
         }
-        return new CommandPair(mRequest == null ? null : (Command) mRequest, (CommandResponse) mResponse);
+        return new CommandPair(mRequest == null ? null : (Command) mRequest,
+                (CommandResponse) mResponse);
     }
 
 }
