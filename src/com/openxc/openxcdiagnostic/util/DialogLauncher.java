b4ec9c81669d9a19ea08277a406dd09e3684954a
Charles Reitz <cgreitz@hotmail.com>
2014-07-28 14:17:45 -0400

    Consolidate alert dialog launching.

--- src/com/openxc/openxcdiagnostic/util/DialogLauncher.java
+++ src/com/openxc/openxcdiagnostic/util/DialogLauncher.java
@@ -5,21 +5,18 @@ import android.app.AlertDialog;
 
 public class DialogLauncher {
 
-    public static void launchAlert(Activity context, String title, String message) {
-        
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setMessage(message);
-        builder.setTitle(title);
-        builder.setPositiveButton("Done", null);
-        builder.create().show();
+    public static void launchAlert(Activity context, String title,
+            String message) {
+        launchAlert(context, title, message, "OK");
     }
-    
-    public static void launchAlert(Activity context, String title, String message, String doneButton) {
+
+    public static void launchAlert(Activity context, String title,
+            String message, String doneButton) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setMessage(message);
         builder.setTitle(title);
         builder.setPositiveButton(doneButton, null);
         builder.create().show();
     }
-    
+
 }
