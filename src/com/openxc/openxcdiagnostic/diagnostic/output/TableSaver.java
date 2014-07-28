5ade88a8050aa2481e954c8bf3bcdc0352f95b67
Charles Reitz <cgreitz@hotmail.com>
2014-07-28 13:39:07 -0400

    Run save actions formatting.

--- src/com/openxc/openxcdiagnostic/diagnostic/output/TableSaver.java
+++ src/com/openxc/openxcdiagnostic/diagnostic/output/TableSaver.java
@@ -2,6 +2,7 @@ package com.openxc.openxcdiagnostic.diagnostic.output;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
@@ -22,85 +23,85 @@ public class TableSaver {
     private ArrayList<DiagnosticPair> diagnosticPairs;
     private ArrayList<CommandPair> commandPairs;
     private static SharedPreferences sPreferences;
-    
+
     public TableSaver(DiagnosticActivity context) {
         sPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         diagnosticPairs = loadDiagnosticPairs();
         commandPairs = loadCommandPairs();
     }
-    
+
     public void saveDiagnosticRows(ArrayList<OutputRow> rows) {
         diagnosticPairs = new ArrayList<>();
-        for (int i=0; i < rows.size(); i++) {
+        for (int i = 0; i < rows.size(); i++) {
             diagnosticPairs.add((DiagnosticPair) rows.get(i).getPair());
         }
         saveDiagnosticPairs();
     }
-    
+
     public void saveCommandRows(ArrayList<OutputRow> rows) {
         commandPairs = new ArrayList<>();
-        for (int i=0; i < rows.size(); i++) {
+        for (int i = 0; i < rows.size(); i++) {
             commandPairs.add((CommandPair) rows.get(i).getPair());
         }
         saveCommandPairs();
     }
-    
+
     public ArrayList<DiagnosticPair> getDiagnosticPairs() {
         return diagnosticPairs;
     }
-   
+
     public ArrayList<CommandPair> getCommandPairs() {
         return commandPairs;
     }
-    
-    private void saveDiagnosticPairs() {  
+
+    private void saveDiagnosticPairs() {
         save(getSavedDiagnosticRowsKey(), (new Gson()).toJson(diagnosticPairs));
     }
-    
+
     private void saveCommandPairs() {
         save(getSavedCommandRowsKey(), (new Gson()).toJson(commandPairs));
     }
-    
+
     private void save(String key, String jsonString) {
         Editor prefsEditor = sPreferences.edit();
         prefsEditor.putString(key, jsonString);
         prefsEditor.commit();
     }
-    
+
     private ArrayList<CommandPair> loadCommandPairs() {
-        
+
         @SuppressWarnings("serial")
-        Type type = new TypeToken<ArrayList<CommandPair>>(){}.getType();
+        Type type = new TypeToken<ArrayList<CommandPair>>() {
+        }.getType();
         String json = sPreferences.getString(getSavedCommandRowsKey(), "");
-        ArrayList<CommandPair> rowList 
-            = (new Gson()).fromJson(json, type);
+        ArrayList<CommandPair> rowList = (new Gson()).fromJson(json, type);
         if (rowList != null) {
             return new ArrayList<>(rowList);
         }
-        
+
         return new ArrayList<>();
     }
-    
+
     private ArrayList<DiagnosticPair> loadDiagnosticPairs() {
-        
+
         @SuppressWarnings("serial")
-        Type type = new TypeToken<ArrayList<DiagnosticPair>>(){}.getType();
+        Type type = new TypeToken<ArrayList<DiagnosticPair>>() {
+        }.getType();
         String json = sPreferences.getString(getSavedDiagnosticRowsKey(), "");
-        ArrayList<DiagnosticPair> rowList 
-            = (new Gson()).fromJson(json, type);
+        ArrayList<DiagnosticPair> rowList = (new Gson()).fromJson(json, type);
         if (rowList != null) {
             return new ArrayList<>(rowList);
         }
-        
+
         return new ArrayList<>();
     }
-    
+
     private static String getSavedCommandRowsKey() {
         return "saved_command_rows_key";
     }
-    
+
     private static String getSavedDiagnosticRowsKey() {
         return "saved_diagnostic_rows_key";
     }
-    
+
 }
